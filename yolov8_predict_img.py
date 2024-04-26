10aa5697a4bdd4299906307a5652b882f50b7a7a
openytu <168019957+openytu@users.noreply.github.com>
2024-04-26 15:35:26 +0300

    Update yolov8_predict_img.py

--- yolov8_predict_img.py
+++ yolov8_predict_img.py
@@ -31,7 +31,6 @@ results = model.track(img, persist=True, verbose = False)[0]
 bboxes = np.array(results.boxes.data.tolist(), dtype = "int")
 
 for box in bboxes:
-        
     x1, y1, x2, y2, track_id, score, class_id = box
     img_roi = img[y1:y2, x1:x2]
     text_detections = reader.readtext(img_roi)
@@ -39,10 +38,12 @@ for box in bboxes:
     for boxes, text, score_text in text_detections:
         print(text)
         text = "ID: {}  PLATE: {}".format(track_id, text)
-        cv2.putText(img, text, (x1, y1 -5), font, font_scale, (0,255,0), thickness, cv2.LINE_AA)
-        cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,0), thickness)
-        cv2.imshow("img",img)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        cv2.putText(img, text, (x1, y1 - 5), font, font_scale, (0, 255, 0), thickness, cv2.LINE_AA)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), thickness)
+        
+    
+cv2.imshow("img", img)
+cv2.waitKey(0) 
+cv2.imwrite(f"results/result.jpg", img)  
+    
+cv2.destroyAllWindows()  
